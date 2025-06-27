45a1a34cfea61c7d022f93ae938b0c2c55bbb3dd
Nakroth-5 <evert.aov@gmail.com>
2025-06-27 11:32:32 -0400

    Mejoras

--- AutomatizacionMetodosGranMYDosFases/src/main/java/org/universidad/automatizacionmetodosgranmydosfases/HelloApplication.java
+++ AutomatizacionMetodosGranMYDosFases/src/main/java/org/universidad/automatizacionmetodosgranmydosfases/HelloApplication.java
@@ -8,11 +8,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
-    public static void main(String[] args) {
-
-    }
-    
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -20,5 +15,9 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
